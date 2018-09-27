5125d7ef9cd59560c04f103ed04da642c833d745
Gowtham Anbazhagan <gowthama@tag124.tag.dc>
2018-09-27 19:13:45 +0530

    removed ngbmodule  which was missed during merge conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './core/containers/app/app.component';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
@@ -13,7 +12,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     AuthModule,
     AppRoutingModule,
     CoreModule,
-    NgbModule.forRoot()
   ],
   bootstrap: [AppComponent]
 })
