a97e2935720cfe9bed1ff2e36994207c102cad60
Gowtham Anbazhagan <gowthama@tag124.tag.dc>
2018-09-27 17:05:33 +0530

    added bootstrap, TODO include jquery,popper,bootstrap js in angular.json when creating the project if necessary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './core/containers/app/app.component';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     BrowserModule,
     AuthModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    NgbModule.forRoot()
   ],
   bootstrap: [AppComponent]
 })
